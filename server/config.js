60cea50d5564cdd59d4ceecb4a5022a19cbf9b90
mrnemeth <adamnemeth.contact@gmail.com>
2018-03-28 00:07:07 +0200

    connected atlas mongodb

--- server/config.js
+++ server/config.js
@@ -1,4 +1,4 @@
 module.exports = {
   'secret': 'bippidibuppi',
-  'database': 'mongodb://localhost/MyDatabase'
+  'database': 'mongodb://adam-nmth:IVcXmLM2fjFb7lBS@cluster0-shard-00-00-sq90f.mongodb.net:27017,cluster0-shard-00-01-sq90f.mongodb.net:27017,cluster0-shard-00-02-sq90f.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin'
 }
